1586ab35530bb464740d201a8ceed4e0a1ca4955
asharmalik <asharm95@gmail.com>
2014-10-18 18:34:31 -0500

    Added random facts

--- CatBlock.js
+++ CatBlock.js
@@ -1,2 +1,2 @@
-$( "#ad_main" ).replaceWith( "<a href='http://thecatapi.com'><img src='http://thecatapi.com/api/images/get?format=src&type=gif'></a>");
-//$( "#ad_main" ).remove();
+//$( "#ad_main" ).replaceWith( "<a href='http://thecatapi.com'><img src='http://thecatapi.com/api/images/get?format=src&type=gif'></a>");
+$( "#ad_main" ).remove();
