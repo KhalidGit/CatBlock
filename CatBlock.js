bbc543c8fc8edad1352ebce61d9417342c76dd0f
Yash Parekh <yparekh39@gmail.com>
2014-10-18 17:27:22 -0500

    fixed replace, added https to manifest

--- CatBlock.js
+++ CatBlock.js
@@ -1,1 +1,2 @@
-$( "#ad_main" ).replaceWith( "<img src='http://thecatapi.com/api/images/get?format=src&type=gif'></img>" );
+$( "#ad_main" ).replaceWith( "<a href='http://thecatapi.com'><img src='http://thecatapi.com/api/images/get?format=src&type=gif'></a>");
+//$( "#ad_main" ).remove();
