23479adad829a1e7c95802efef2aabfb9dc1cfc3
asharmalik <asharm95@gmail.com>
2014-10-19 02:49:53 -0500

    Added more ad cases

--- CatBlock.js
+++ CatBlock.js
@@ -1,9 +1,12 @@
-var selectors = ["#ad_main", "[id*='-ad']:first"];
+var selectors = ['*[id^="ad"]', "[id*='-ad']", "[id*='_ad']", "[id*=' ad']", '*[class^="ad"]'];
 var i = 0;
-var j = 0;
+
+for(i=0;i<selectors.length;i++){
+    selectors[i] = selectors[i]+":first";
+}
 
 
-
+//do we want to make the width set a different way other than constant 300?
 for(i=0;i<selectors.length;i++) {
     while($(selectors[i]).length){
         $(selectors[i]).replaceWith("<img src=\"http://thecatapi.com/api/images/get?format=src&type=gif&size=med&dummy="+Math.random()+"\" width=\"300px\">");
